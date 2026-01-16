71c21fe028d1a6a2988e3a144c407b5c37874f28
TechRiderWR <techrider@wingriders.com>
2026-01-16 09:02:59 +0100

    [BE] Initialize tx submission client (#19)

--- backend/src/agent/index.ts
+++ backend/src/agent/index.ts
@@ -1,14 +1,10 @@
 import {ensure} from '@wingriders/multi-dex-launchpad-common'
 import {config, isAgentMode} from '../config'
 import {ensureDbMigrated} from '../db/migrate-db'
-import {prisma} from '../db/prisma-client'
-import {logger} from '../logger'
 import {startChainSyncClient} from './ogmios/chain-sync'
-import {getOgmiosContext} from './ogmios/ogmios'
 
 export const startAgent = async () => {
   ensure(isAgentMode, {mode: config.MODE}, 'Unsupported MODE')
-  await getOgmiosContext()
   await ensureDbMigrated()
   startChainSyncClient()
 }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,13 @@
 import {startAgent} from './agent'
+import {getOgmiosContext} from './agent/ogmios/ogmios'
+import {initializeTxSubmissionClient} from './agent/ogmios/tx-submission-client'
 import {isAgentMode} from './config'
 import {startServer} from './server'
 
 const start = async () => {
   startServer()
+  await getOgmiosContext()
+  initializeTxSubmissionClient()
   if (isAgentMode) startAgent()
 }
 
