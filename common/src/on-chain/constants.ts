cba098c3c88a460536b5aa5a35cd0bc36a56a28f
TechRiderWR <techrider@wingriders.com>
2026-01-28 08:21:19 +0100

    [COMMON] Set valid values for constants (#59)

--- common/src/on-chain/constants.ts
+++ common/src/on-chain/constants.ts
@@ -33,18 +33,19 @@ export const DAO_FEE_DENOMINATOR = 1000
 
 export const DAO_FEE_RECEIVER_BECH32_ADDRESS = {
   mainnet: 'todo',
-  preprod: 'todo',
+  preprod:
+    'addr_test1qzuz2e6p4z54thrj0u5nnpg5523jkmnvqjtu78p6873daddnq89ztzv3zmk7lyr9krkt4dpdaw37x5v3daf5muspw0rqwhnlz5',
 }
 
 // TODO: that should be the agent
 export const DAO_ADMIN_PUB_KEY_HASH = {
   mainnet: 'todo',
-  preprod: 'todo',
+  preprod: 'b8256741a8a955dc727f29398514a2a32b6e6c0497cf1c3a3fa2deb5',
 }
 
-export const NODE_ADA = ''
-export const COMMIT_FOLD_FEE_ADA = ''
-export const OIL_ADA = ''
+export const NODE_ADA = '2000000'
+export const COMMIT_FOLD_FEE_ADA = '2000000'
+export const OIL_ADA = '6000000'
 
 // 1000 ADA
 export const LAUNCH_COLLATERAL = '1000000000'
@@ -58,8 +59,8 @@ export const VESTING_VALIDATOR_HASH = getScriptFromExport(
 ).hash
 
 export const WR_FACTORY_VALIDATOR_HASH = {
-  mainnet: 'todo',
-  preprod: 'todo',
+  mainnet: 'c57b588ebda735d49008d47afde48198ec37062d6ceb758803515013',
+  preprod: 'a1640449d34fdfefc56868f0262eda2bebf91b33d1cda381dbfe3e2d',
 }
 
 export const WR_POOL_VALIDATOR_HASH = {
@@ -79,6 +80,7 @@ export const SUNDAE_POOL_SCRIPT_HASH = {
 }
 
 export const SUNDAE_SETTINGS_SYMBOL = {
-  mainnet: 'todo',
-  preprod: 'todo',
+  mainnet: '6d9d7acac59a4469ec52bb207106167c5cbfa689008ffa6ee92acc50',
+  // TODO: I couldn't find the preprod one
+  preprod: '6d9d7acac59a4469ec52bb207106167c5cbfa689008ffa6ee92acc50',
 }
